341198f8ae43111cb90dcc4ff1499bd396f35069
David Elmo Ross <110311948+DavidElmoRoss@users.noreply.github.com>
2025-04-25 15:49:55 -0400

    Update Grove_LCD_RGB_Backlight.cpp
    changed ms to us on line 47

--- Grove_LCD_RGB_Backlight.cpp
+++ Grove_LCD_RGB_Backlight.cpp
@@ -44,7 +44,7 @@ Grove_LCD_RGB_Backlight::Grove_LCD_RGB_Backlight(PinName sda, PinName scl) : i2c
  
    //Wait for more than 30 ms after power rises above 4.5V per the data sheet
    // wait_ms(50);
-  wait_ms(50);
+  wait_us(50000);
  
  
     // Send first function set command. Wait longer that 39 us per the data sheet
